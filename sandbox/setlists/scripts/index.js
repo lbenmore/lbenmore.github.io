bbf24ba8b84cc6c80e8f65723b33eb6afa328cf7
Lisa Benmore <lbenmore@gmail.com>
2026-09-22 15:59:07 -0700

    restructure content creation

--- sandbox/setlists/scripts/index.js
+++ sandbox/setlists/scripts/index.js
@@ -15,52 +15,96 @@ const parseDate = (ts) => {
   return `${months[m]} ${d}, ${y}`;
 };
 
-  const init = ([template, ...lists]) => {
-    const markup = lists.map((list, idx) => (
-      `<div class="card" style="--setlist-image: url('../assets/${list.thumbnail}')">
-        <input class="card__toggle" type="radio" name="cards" value="card-${idx}" id="card-${idx}" hidden>
-        <div class="card__wrapper">
-          <div class="card__inner">
-            <div class="card__side card__side--front">
-            <label class="card__label" for="card-${idx}">
-                <div class="card__image"></div>
-                <div class="card__title">${list.label}</div>
-              </label>
-            </div>
-            <div class="card__side card__side--back">
-              <label>
-                Back
-                <input type="radio" name="cards" value="none" hidden>
-              </label>
-              <h1 class="card__title">${list.label}</h1>
-              <p>${parseDate(list.timestamp)}</p>
-              <ul>
-                ${list.artists.map((artist) => (
-                  `<li class="card__artist">${artist}</li>`
-                )).join("")}
-              </ul>
-              ${list.sets.map((set) => (
-                `<h2 class="card__artist">${set.artist}</h2>
-                <ol class="card__setlist">
-                  ${set.songs.map((song) => (
-                    `<li class="card__song">${song}</li>`
-                  )).join("")}
-                </ol>`
-              )).join("")}
-            </div>
-          </div>
+const generateCard = (setlistData, idx) => {
+  const card = document.createElement("div");
+  const cardMarkup = (
+    `<input class="card__toggle" type="radio" name="cards" value="card-${idx}" id="card-${idx}" hidden>
+      <div class="card__wrapper">
+        <div class="card__inner">
+          <div class="card__side card__side--front">
+          <label class="card__label" for="card-${idx}">
+            <div class="card__image"></div>
+            <div class="card__title">${setlistData.label}</div>
+          </label>
         </div>
-      </div>`
-    ).join("");
+        <div class="card__side card__side--back">
+          <label>
+            Back
+            <input type="radio" name="cards" value="none" hidden>
+          </label>
+          <h1 class="card__title">${setlistData.label}</h1>
+          <p>${parseDate(setlistData.timestamp)}</p>
+          <ul>
+            ${setlistData.artists.map((artist) => (
+              `<li class="card__artist">${artist}</li>`
+            )).join("")}
+          </ul>
+          ${setlistData.sets.map((set) => (
+            `<h2 class="card__artist">${set.artist}</h2>
+            <ol class="card__setlist">
+              ${set.songs.map((song) => (
+                `<li class="card__song">${song}</li>`
+              )).join("")}
+            </ol>`
+          )).join("")}
+        </div>
+      </div>
+    </div>`
+  );
 
-  x._(".cards").innerHTML = markup;
-  document.documentElement.style.setProperty("--scrollbar-buffer", `${window.innerWidth - document.documentElement.clientWidth}px`);
+  card.style.setProperty("--setlist-image", `url('../assets/${setlistData.thumbnail}')`);
+  card.classList.add("card");
+  card.innerHTML = cardMarkup;
+
+  return card;
+}
+
+const handleSetlistsData = ([template, ...setlistsData]) => {
+  const cards = setlistsData.map((setlistData, idx) => generateCard(setlistData, idx));
+  const target = x._(".cards");
+  cards.forEach((card) => target.appendChild(card));
+
+  return true;
 };
 
-fetch("./data.json")
-  .then((res) => res.json())
-  .then((json) => init(json))
-  .catch((err) => console.error("something has gone awry!", err));
+const updateStyling = () => {
+  // set the scrollbar offset CSS variable value
+  try {
+    const { innerWidth } = window;
+    const { documentElement: docEl } = document;
+    const { clientWidth } = docEl;
+    const scrollbarOffsetValue = innerWidth - clientWidth;
+
+    docEl.style.setProperty("--scrollbar-offset", `${scrollbarOffsetValue}px`);
+  } catch (err) {
+    console.error("Error updating styling:");
+    console.error(err);
+  }
+}
+
+async function populateCards() {
+  await fetch("./data.json")
+    .then((response) => response.json())
+    .then((setlistsData) => handleSetlistsData(setlistsData))
+    .catch((err) => console.error("something has gone awry!", err));
+
+  return true;
+}
+
+async function initFns() {
+  await populateCards();
+  updateStyling();
+}
+
+if (document.readyState === "complete") {
+  initFns();
+} else {
+  document.addEventListener("readystatechange", () => {
+    if (document.readyState === "complete") {
+      initFns();
+    }
+  });
+}
 
 try {
   fetch("./assets/doing_a_tool_thing.txt")
